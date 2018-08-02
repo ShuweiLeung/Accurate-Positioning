01d9b69c289fa09200d839294691821dc5e9d1b4
Shuwei Liang <36534502+ShuweiLeung@users.noreply.github.com>
2018-08-02 14:21:46 +0800

    Update positioning.py

--- positioning.py
+++ positioning.py
@@ -14,27 +14,24 @@ class Geo:
    	    obj = json.loads(line)
         ip = obj["ip"]
 				
-				#load external database to obtain geographical info
+	#load external database to obtain geographical info
 				
-				# This creates a Reader object. You should use the same object across multiple requests as creation of it is expensive.
-				reader = geoip2.database.Reader('your path to GeoLite2-City.mmdb')
+	# This creates a Reader object. You should use the same object across multiple requests as creation of it is expensive.
+	reader = geoip2.database.Reader('your path to GeoLite2-City.mmdb')
 				
-				#Replace "city" with the method corresponding to the database that you are using, e.g., "country".
-				geo = reader.city(ip)
-				
-				country_iso_code = geo.country.iso_code  #'US'
-				country = geo.country.name  #United States
-				country_Chinese = geo.country.names['zh-CN']  #u'美国'
-				state = geo.subdivisions.most_specific.name  #'Minnesota'
-				state_iso_code = geo.subdivisions.most_specific.iso_code  #'MN'
-				city = geo.city.name  #'Minneapolis'
-				postcode = geo.postal.code  #'55455'
-				latitude = geo.location.latitude  #'44.9733'
-				longitude = geo.location.longitude  #'-93.2323'
-				
-				reader.close()
+	#Replace "city" with the method corresponding to the database that you are using, e.g., "country".
+        geo = reader.city(ip)
+
+        country_iso_code = geo.country.iso_code  #'US'
+        country = geo.country.name  #United States
+        country_Chinese = geo.country.names['zh-CN']  #u'美国'
+        state = geo.subdivisions.most_specific.name  #'Minnesota'
+        state_iso_code = geo.subdivisions.most_specific.iso_code  #'MN'
+        city = geo.city.name  #'Minneapolis'
+        postcode = geo.postal.code  #'55455'
+        latitude = geo.location.latitude  #'44.9733'
+        longitude = geo.location.longitude  #'-93.2323'
+
+        reader.close()
     
-		input.close()
-				
-				
-    
+    input.close()
